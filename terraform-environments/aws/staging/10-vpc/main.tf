67e441f7659a3735099ae641c7dac387f5b0019f
stefanruci1997 <stefanruci@hotmail.com>
2024-03-21 12:46:33 +0100

    fix vpc

--- terraform-environments/aws/staging/10-vpc/main.tf
+++ terraform-environments/aws/staging/10-vpc/main.tf
@@ -39,7 +39,7 @@ module "vpc" {
   source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/vpc?ref=v1.0.30"
 
   aws_region       = local.aws_region
-  azs              = ["us-east-1a", "us-east-1c", "us-east-1d"]
+  azs              = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
   vpc_cidr         = "10.0.0.0/16"
   private_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets   = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
